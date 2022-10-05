a995e6b689d3eca6e56903f99eff558d5a911691
JTT1 <jt.teppo@gmail.com>
2022-10-05 13:03:42 +0300

    Menu updates

---
+++ Assets/Scripts/MenuScripts/PauseMenu.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+
+    public static bool gameIsPaused = false;
+    public GameObject PauseMenuUi;
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    public void Resume ()
+    {
+        PauseMenuUi.SetActive(false);
+        Time.timeScale = 1f;
+        gameIsPaused = false;
+    }
+
+    public void Pause ()
+    {
+        PauseMenuUi.SetActive(true);
+        Time.timeScale = 0f;
+        gameIsPaused = true;
+    }
+
+    public void GoMenu ()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(0);
+    }
+
+
+
+}
