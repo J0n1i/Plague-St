a68ea3adafd734c6f8e38df8b7e2d5f7ecd30d64
JTT1 <jt.teppo@gmail.com>
2022-10-19 12:52:29 +0300

    settings menu säätöä

--- Assets/Scripts/MenuScripts/SettingsMenu.cs
+++ Assets/Scripts/MenuScripts/SettingsMenu.cs
@@ -1,13 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SettingsMenu : MonoBehaviour
 {
 
     public GameObject MainMenu;
     public GameObject ModifyMenu;
+    public Slider sliderVolume;
+    public float VolumeLevel;
+    public Text VolumeText;
+    public string Volume;
 
+
+    void Start()
+    {
+        VolumeText = GetComponent<Text>();
+        VolumeBar();
+    }
     public void ShowSettings ()
     {
         MainMenu.SetActive(false);
@@ -20,6 +31,17 @@ public class SettingsMenu : MonoBehaviour
         ModifyMenu.SetActive(false);
     }
 
+    public void VolumeBar ()
+    {
+        VolumeLevel = sliderVolume.value;
+        Debug.Log(VolumeLevel);
+        Volume = VolumeLevel.ToString();
+    }
+
+    public void MusicBar()
+    {
+
+    }
 
 
 
