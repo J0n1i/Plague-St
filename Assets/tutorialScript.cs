e591281b7a668a6ba7fd186636b360030c80f4c1
Joona Haataja <joona00@windowslive.com>
2022-11-30 15:35:43 +0100

    tutoriaali huone + scriptattu tutoriaali + kamera korjaus

--- Assets/tutorialScript.cs
+++ Assets/tutorialScript.cs
@@ -14,13 +14,9 @@ public class tutorialScript : MonoBehaviour
     public GameObject chest;
     public GameObject chestSquare;
     public Image image4;
-    public Image image5;
-    public Image image6;
-    public Image image7;
     public Image image8;
     public Image image9;
-    public GameObject icon;
-    public GameObject icon2;
+    public Image image10;
     public GameObject door;
     // Start is called before the first frame update
     void Start()
@@ -47,14 +43,10 @@ public class tutorialScript : MonoBehaviour
                 coinholder.SetActive(false);
                 chestSquare.SetActive(false);
                 image4.enabled = false;
-                image5.enabled = true;
-                image6.enabled = true;
-                image7.enabled = true;
                 image8.enabled = true;
                 image9.enabled = true;
+                image10.enabled = true;
                 door.SetActive(false);
-                icon.SetActive(true);
-                icon2.SetActive(true);
         }
     }
 }
